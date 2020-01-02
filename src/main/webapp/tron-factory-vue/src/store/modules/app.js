4e352b65ff9100444b3578ad7099f4e5cfd50505
康建城 <1063303665@qq.com>
2020-01-02 17:03:08 +0800

    refactor: refactor the vuex for the menu routes

--- src/main/webapp/tron-factory-vue/src/store/modules/app.js
+++ src/main/webapp/tron-factory-vue/src/store/modules/app.js
@@ -1,60 +1,81 @@
-import Cookies from 'js-cookie'
-import { menuRoutes } from '@/router'
+import {
+  allMenuRoutes,
+  authMenuRoutes
+} from '@/router'
 
+const authMenu = JSON.parse(sessionStorage.getItem('authMenu') || '[]')
 
-const state = {
-  menuRoutes: [...menuRoutes],
-  sidebar: {
-    opened: Cookies.get('sidebarStatus') ? !!+Cookies.get('sidebarStatus') : true,
-    withoutAnimation: false,
-  },
-  isCollapseAside: localStorage.getItem('isCollapseAside') === 'true',
-  device: '',
-}
+// init the disabled status for auth menu routes
+function initAuthMenuRoutes (authMenuRoutes, authMenu, isInit = true) {
+  return authMenuRoutes.map(route => {
 
-const getters = {
-  menuRoutes (state) {
-    return state.menuRoutes
-  },
-  isCollapseAside (state) {
-    return state.isCollapseAside
-  },
-}
-
-const mutations = {
-  toggleCollapseAside (state) {
-    state.isCollapseAside = !state.isCollapseAside
-    localStorage.setItem('isCollapseAside', state.isCollapseAside)
-  },
-  TOGGLE_SIDEBAR: state => {
-    state.sidebar.opened = !state.sidebar.opened
-    state.sidebar.withoutAnimation = false
-    if (state.sidebar.opened) {
-      Cookies.set('sidebarStatus', 1)
-    } else {
-      Cookies.set('sidebarStatus', 0)
+    if (route.meta && ~authMenu.findIndex(name => route.meta.name === name)) {
+      route.meta.disabled = false
+    } else if (!isInit) {
+      // after init, if not exist in auth menu that is disabled true
+      route.meta.disabled = true
     }
-  },
-  CLOSE_SIDEBAR: (state, withoutAnimation) => {
-    Cookies.set('sidebarStatus', 0)
-    state.sidebar.opened = false
-    state.sidebar.withoutAnimation = withoutAnimation
-  },
+
+    return route
+  })
 }
 
-const actions = {
-  toggleSideBar({ commit }) {
-    commit('TOGGLE_SIDEBAR')
-  },
-  closeSideBar({ commit }, { withoutAnimation }) {
-    commit('CLOSE_SIDEBAR', withoutAnimation)
-  },
+// init auth menu
+function initAuthMenu (authMenuRoutes, authMenu) {
+  authMenuRoutes.forEach(route => {
+    if (route.meta && !route.meta.disabled && !authMenu.includes(route.meta.name)) {
+      authMenu.push(route.meta.name)
+    }
+  })
+
+  sessionStorage.setItem('authMenu', JSON.stringify(authMenu))
+
+  return authMenu
 }
 
 export default {
   namespaced: true,
-  state,
-  getters,
-  mutations,
-  actions,
+
+  state: {
+    isCollapseAside: localStorage.getItem('isCollapseAside') === 'true',
+    allMenuRoutes: allMenuRoutes,
+    authMenuRoutes: initAuthMenuRoutes(authMenuRoutes, authMenu),
+    authMenu: initAuthMenu(authMenuRoutes, authMenu),
+  },
+
+  getters: {
+    isCollapseAside (state) {
+      return state.isCollapseAside
+    },
+    authMenuRoutes (state) {
+      return state.authMenuRoutes
+    },
+    authMenu (state) {
+      return state.authMenu
+    },
+    allMenuRoutes (state) {
+      return state.allMenuRoutes
+    },
+  },
+
+  mutations: {
+    // name is the meta name of the route path
+    SET_AUTH_MENU (state, { nameList, name }) {
+      state.authMenu = nameList || state.authMenu
+
+      if (name && !state.authMenu.includes(name)) {
+        state.authMenu.push(name)
+      }
+
+      initAuthMenuRoutes(state.authMenuRoutes, state.authMenu, false)
+      initAuthMenu(state.authMenuRoutes, state.authMenu)
+    },
+
+    toggleCollapseAside (state) {
+      state.isCollapseAside = !state.isCollapseAside
+      localStorage.setItem('isCollapseAside', state.isCollapseAside)
+    },
+  },
+
+  actions: {},
 }
