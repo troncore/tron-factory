c188877e680335c3fffa654f7ffe51e86ce2ef84
康建城 <1063303665@qq.com>
2020-01-09 11:38:44 +0800

    refactor: transfer en

--- src/main/webapp/tron-factory-vue/src/locale/lang/en-US/package/importPlugin.js
+++ src/main/webapp/tron-factory-vue/src/locale/lang/en-US/package/importPlugin.js
@@ -1,16 +1,21 @@
 export default {
-  consensusModule: '共识模块',
-  transactionModule: '交易模块',
+  sshUserName: 'SSH 用户名',
+  isSR: '是否为 SR',
 
-  moreSetting: '高级设置',
-  customTransactionModule: '自定义交易模块',
-
+  batchDeploy: '批量部署',
+  deployDialogTitle: '批量部署',
+  deployingTips: '正在部署,请稍候,点击列表查看详情查看部署状态',
+  mustSelectNodes: '至少选择一个节点进行部署',
+  deployHelpTips: '批量部署前首先需要填写本地jar包文件路径 如：/Users/xxx/xxx/java-tron-1.0.0.zip',
   valid: {
-    checkCustomTransaction: '请勾选自定义交易模块',
-    inputCustomTransaction: '请填写自定义交易模块路径（以.jar结束）',
-    pathEndJAR: '请填写以.jar为结尾的路径',
+    inputFilePath: '请输入jar包路径',
+    inputRightPath: '请输入以java-tron-1.0.0.zip为结尾的路径',
   },
 
-  consensusModuleSaveSuccess: '保存共识模块成功',
-  transactionModuleSaveSuccess: '保存交易模块成功',
+  viewLog: '查看日志',
+  logDialogTitle: '节点日志',
+  emptyLog: '当前部署暂无日志',
+  loadingLog: '正在查询节点记录，请稍等',
+  deployComplete: '部署完成',
+  deployFail: '部署完成',
 }
