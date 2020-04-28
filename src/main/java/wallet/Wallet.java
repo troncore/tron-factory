cb76105814695ff6890b1706e9b6a28422e7f808
Aiden <Aiden.Liu@tron.network>
2020-04-28 18:03:08 +0800

    catch exception:genesisSettingConfig

--- src/main/java/wallet/Wallet.java
+++ src/main/java/wallet/Wallet.java
@@ -195,22 +195,27 @@ public class Wallet {
     return Base58.encode(inputCheck);
   }
   public static byte[] decode58Check(String input) {
-    byte[] decodeCheck = Base58.decode(input);
-    if (decodeCheck.length <= 4) {
+    try{
+      byte[] decodeCheck = Base58.decode(input);
+      if (decodeCheck.length <= 4) {
+        return null;
+      }
+      byte[] decodeData = new byte[decodeCheck.length - 4];
+      System.arraycopy(decodeCheck, 0, decodeData, 0, decodeData.length);
+      // shoud be "true" for consistency
+      byte[] hash0 = Sha256Sm3Hash.hash(decodeData);
+      byte[] hash1 = Sha256Sm3Hash.hash(hash0);
+      if (hash1[0] == decodeCheck[decodeData.length] &&
+              hash1[1] == decodeCheck[decodeData.length + 1] &&
+              hash1[2] == decodeCheck[decodeData.length + 2] &&
+              hash1[3] == decodeCheck[decodeData.length + 3]) {
+        return decodeData;
+      }
+      return null;
+
+    }catch(IllegalArgumentException ie){
       return null;
     }
-    byte[] decodeData = new byte[decodeCheck.length - 4];
-    System.arraycopy(decodeCheck, 0, decodeData, 0, decodeData.length);
-    // shoud be "true" for consistency
-    byte[] hash0 = Sha256Sm3Hash.hash(decodeData);
-    byte[] hash1 = Sha256Sm3Hash.hash(hash0);
-    if (hash1[0] == decodeCheck[decodeData.length] &&
-        hash1[1] == decodeCheck[decodeData.length + 1] &&
-        hash1[2] == decodeCheck[decodeData.length + 2] &&
-        hash1[3] == decodeCheck[decodeData.length + 3]) {
-      return decodeData;
-    }
-    return null;
   }
 
   public static byte[] decodeFromBase58Check(String addressBase58) {
