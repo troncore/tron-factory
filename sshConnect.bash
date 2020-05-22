9ab6b98bd06fe5a29a3de3f859849421cb7ddc7a
Aiden <Aiden.Liu@tron.network>
2020-05-22 13:14:56 +0800

    modify file: sshConnect.bash

--- sshConnect.bash
+++ sshConnect.bash
@@ -4,8 +4,10 @@ echo "Start ssh deployment"
 finish="deploy finish"
 noCheck="StrictHostKeyChecking no"
 
-ssh -p $2 $3@$1 exit
-if [ $? = 0 ];then
+ssh -p $2 $3@$1 -o "${noCheck}" exit
+result=$?
+echo $result
+if [ $result = 0 ];then
   echo "ssh connect successfully"
 else
   echo "ssh connect failed"
