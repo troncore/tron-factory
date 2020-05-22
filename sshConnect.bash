835736f913c478f3047d120209f4996887355610
Aiden <Aiden.Liu@tron.network>
2020-05-22 13:24:33 +0800

    modify file

--- sshConnect.bash
+++ sshConnect.bash
@@ -3,7 +3,9 @@ APP="java-tron-1.0.0"
 echo "Start ssh deployment"
 finish="deploy finish"
 noCheck="StrictHostKeyChecking no"
-
+echo $1
+echo $2
+echo $3
 ssh -p $2 $3@$1 -o "${noCheck}" exit
 result=$?
 echo $result
