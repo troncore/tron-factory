3d813ce1aba507ece6ffadcd3e55845ea2316035
Aiden <Aiden.Liu@tron.network>
2020-07-24 15:03:54 +0800

    fix bug:stopnode

--- stopNode.bash
+++ stopNode.bash
@@ -5,7 +5,7 @@ echo "[$time] start stopNode"
 #获取远程机器的pid
 pid=`head -1 /tmp/startPid-$4`
 echo $pid
-result=`kill -9 $pid`
+result=`ssh -p $2 $3@$1 "kill -9 $pid"`
 if [ -z $result ];then
   time=$(date "+%Y-%m-%d %H:%M:%S")
   echo "[$time] stop node successfully"
